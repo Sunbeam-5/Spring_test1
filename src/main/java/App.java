6c6bf4506028891ef1259a95b3bd9e22148e11be
Sunbeam-5 <90610326+Sunbeam-5@users.noreply.github.com>
2023-09-18 20:04:59 +0800

    review

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,10 @@
 import com.demo.ay.Controller.UserController;
+import com.demo.ay.component.UserComponent;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class App {
     public static void main(String[] args) {
@@ -10,6 +14,11 @@ public class App {
 
         System.out.println(userController.getUser());
 
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring-config.xml"));
+
+        UserComponent userComponent = (UserComponent) applicationContext.getBean("userComponent");
+
+        System.out.println(userComponent.getUser());
     }
 
 }
--- src/main/java/com/demo/ay/component/UserComponent.java
+++ src/main/java/com/demo/ay/component/UserComponent.java
@@ -1,5 +1,21 @@
 package com.demo.ay.component;
 
+import com.demo.ay.Controller.UserController;
+import com.demo.ay.module.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserComponent {
 
+    private UserController userController;
+
+    @Autowired
+    public UserComponent(UserController userController) {
+        this.userController = userController;
+    }
+
+    public User getUser() {
+        return userController.getUser();
+    }
 }
